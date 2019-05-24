a6fedafe21db29b7a670ef35cac7e1ce86321563
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-05-24 13:00:03 +0100

    CEDS-1218: need to drop the Z from the end of the datetime as WCO dec wont accept it otherwise

--- test/forms/DepartureDetailsSpec.scala
+++ test/forms/DepartureDetailsSpec.scala
@@ -26,9 +26,9 @@ class DepartureDetailsSpec extends BaseSpec {
 
     "format the date correctly" when{
 
-      "date is" in {
+      "date is in ISO 8601 format " in {
         val inputData = DepartureDetails(Date(Some(1), Some(1), Some(2019)))
-        inputData.toString must be("2019-01-01T00:00Z")
+        inputData.toString must be("2019-01-01T00:00")
       }
     }
 

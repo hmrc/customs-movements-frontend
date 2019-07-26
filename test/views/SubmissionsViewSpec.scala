fb90e2b898eaf5ef7ee4530f285d61de1b969194
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-07-26 15:07:55 +0100

    CEDS-1072 Add link to Notifications from Submissions page
    Fix the update date formatter as well

--- test/views/SubmissionsViewSpec.scala
+++ test/views/SubmissionsViewSpec.scala
@@ -16,7 +16,11 @@
 
 package views
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import base.ViewValidator
+import models.{Notification, Submission}
 import org.scalatest.{MustMatchers, WordSpec}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -33,17 +37,43 @@ class SubmissionsViewSpec extends WordSpec with MustMatchers with Stubs with Vie
 
     "contains title" in {
 
-      getElementById(page, "title").text() must be(messages("submissions.title"))
+      page.getElementById("title") must containText(messages("submissions.title"))
     }
 
     "contains correct table headers" in {
 
-      getElementById(page, "ucr").text() must be(messages("submissions.ucr"))
-      getElementById(page, "submissionType").text() must be(messages("submissions.submissionType"))
-      getElementById(page, "submissionAction").text() must be(messages("submissions.submissionAction"))
-      getElementById(page, "dateUpdated").text() must be(messages("submissions.dateUpdated"))
-      getElementById(page, "status").text() must be(messages("submissions.status"))
-      getElementById(page, "noOfNotifications").text() must be(messages("submissions.noOfNotifications"))
+      page.getElementById("ucr") must containText(messages("submissions.ucr"))
+      page.getElementById("submissionType") must containText(messages("submissions.submissionType"))
+      page.getElementById("submissionAction") must containText(messages("submissions.submissionAction"))
+      page.getElementById("dateUpdated") must containText(messages("submissions.dateUpdated"))
+      page.getElementById("status") must containText(messages("submissions.status"))
+      page.getElementById("noOfNotifications") must containText(messages("submissions.noOfNotifications"))
+    }
+
+    "contains correct submission data" in {
+      val dateTime = LocalDate.parse("2019-10-31", DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay()
+      val pageWithData: Html = new submissions(mainTemplate)(
+        Seq(
+          (
+            Submission(
+              conversationId = "conversationId",
+              ucr = "4444",
+              submissionType = "M",
+              submissionAction = "Consolidate",
+              dateUpdated = dateTime,
+              status = Some("Cleared")
+            ),
+            Seq(Notification(dateTimeReceived = dateTime, conversationId = "conversationId"))
+          )
+        )
+      )(FakeRequest(), minimalAppConfig, messages)
+
+      getElementById(pageWithData, "ucr-conversationId").text() must be("4444")
+      getElementById(pageWithData, "submissionType-conversationId").text() must be("M")
+      getElementById(pageWithData, "submissionAction-conversationId").text() must be("Consolidate")
+      getElementById(pageWithData, "dateUpdated-conversationId").text() must be("2019-10-31 00:00")
+      getElementById(pageWithData, "status-conversationId").text() must be("Cleared")
+      getElementById(pageWithData, "noOfNotifications-conversationId").text() must be("1")
     }
   }
 }

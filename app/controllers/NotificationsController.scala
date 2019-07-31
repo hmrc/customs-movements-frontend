780cf68a6bb65651401ad30471ef201eadcdb9c7
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2019-07-31 09:51:09 +0100

    Fix for notification page

--- app/controllers/NotificationsController.scala
+++ app/controllers/NotificationsController.scala
@@ -20,7 +20,7 @@ import config.AppConfig
 import connectors.CustomsDeclareExportsMovementsConnector
 import controllers.actions.AuthAction
 import javax.inject.Inject
-import play.api.libs.json.Json
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import views.html.notifications
@@ -33,11 +33,11 @@ class NotificationsController @Inject()(
   mcc: MessagesControllerComponents,
   notifications: notifications
 )(implicit appConfig: AppConfig, ec: ExecutionContext)
-    extends FrontendController(mcc) {
+    extends FrontendController(mcc) with I18nSupport {
 
   def listOfNotifications(conversationId: String): Action[AnyContent] = authenticate.async { implicit request =>
-    connector.fetchNotifications(conversationId).map { notifications =>
-      Ok(Json.toJson(notifications))
+    connector.fetchNotifications(conversationId).map { result =>
+      Ok(notifications(result))
     }
   }
 }

b8c00f399eab14cfb5f6794122cb07cd37c23b4b
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-10-02 10:41:27 +0100

    CEDS-1693 Add translation matcher to View Matchers

--- test/views/base/ViewMatchers.scala
+++ test/views/base/ViewMatchers.scala
@@ -19,6 +19,7 @@ package views.base
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import org.scalatest.matchers.{MatchResult, Matcher}
+import play.api.i18n.Messages
 import play.api.mvc.Call
 
 //noinspection ScalaStyle
@@ -178,6 +179,14 @@ trait ViewMatchers {
     }
   }
 
+  class TranslationKeyMatcher(key: String) extends Matcher[Messages] {
+    override def apply(left: Messages): MatchResult = MatchResult(
+      matches = left.isDefinedAt(key),
+      rawFailureMessage = s"$key is not defined in Messages",
+      rawNegatedFailureMessage = s"$key is defined in Messages"
+    )
+  }
+
   class ChildMatcherBuilder(tag: String) {
     def containingText(text: String) = new ElementContainsChildWithTextMatcher(tag, text)
     def withAttribute(key: String, value: String) = new ElementContainsChildWithAttributeMatcher(tag, key, value)
@@ -206,4 +215,6 @@ trait ViewMatchers {
   def haveGlobalErrorSummary: Matcher[Element] = new ContainElementWithIDMatcher("error-summary-heading")
 
   def haveHref(value: Call): Matcher[Element] = new ElementHasAttributeValueMatcher("href", value.url)
+
+  def haveTranslationFor(key: String): Matcher[Messages] = new TranslationKeyMatcher(key)
 }

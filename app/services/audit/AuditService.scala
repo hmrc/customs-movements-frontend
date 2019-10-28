a0c80a0680a954ca4a8d524958e1314ae0abd111
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-10-28 10:11:00 +0000

    CEDS-1675 Lower case the audit transactionName value
    This is a request from Titi from the TxM team. The opinion is that TxM
    wants to standardise the payloads format.

--- app/services/audit/AuditService.scala
+++ app/services/audit/AuditService.scala
@@ -89,7 +89,7 @@ class AuditService @Inject()(connector: AuditConnector, @Named("appName") appNam
   private def getAuditTags(transactionName: String, path: String)(implicit hc: HeaderCarrier) =
     AuditExtensions
       .auditHeaderCarrier(hc)
-      .toAuditTags(transactionName = s"Export-Declaration-${transactionName}", path = s"customs-declare-exports/${path}")
+      .toAuditTags(transactionName = s"export-declaration-${transactionName.toLowerCase}", path = s"customs-declare-exports/${path}")
 
   private def handleResponse(result: AuditResult, auditType: String) = result match {
     case Success =>

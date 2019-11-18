79f574b99c268f39cefac7dd62158b07e4d856c7
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-11-18 12:55:56 +0000

    CEDS-1958 Summary Controller

--- test/unit/controllers/SummaryControllerSpec.scala
+++ test/unit/controllers/SummaryControllerSpec.scala
@@ -16,33 +16,33 @@
 
 package unit.controllers
 
-import base.MockSubmissionService
 import controllers.SummaryController
 import controllers.storage.FlashKeys
-import forms.Choice
-import forms.Choice.ShutMUCR
+import forms.{Choice, ConsignmentReferences}
+import models.cache.{ArrivalAnswers, DepartureAnswers, MovementAnswers}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{reset, when}
-import play.api.libs.json.{JsObject, JsString, JsValue}
+import play.api.libs.json.{JsObject, JsString}
 import play.api.test.Helpers._
 import play.twirl.api.HtmlFormat
-import uk.gov.hmrc.http.cache.client.CacheMap
-import unit.base.LegacyControllerSpec
+import services.SubmissionService
+import unit.repository.MockCache
 import views.html.summary.{arrival_summary_page, departure_summary_page}
 
 import scala.concurrent.ExecutionContext.global
+import scala.concurrent.Future
 
-class SummaryControllerSpec extends LegacyControllerSpec with MockSubmissionService {
+class SummaryControllerSpec extends ControllerLayerSpec with MockCache {
 
+  private val service = mock[SubmissionService]
   private val mockArrivalSummaryPage = mock[arrival_summary_page]
   private val mockDepartureSummaryPage = mock[departure_summary_page]
 
-  private val controller = new SummaryController(
-    mockAuthAction,
-    mockJourneyAction,
-    mockErrorHandler,
-    mockCustomsCacheService,
-    mockSubmissionService,
+  private def controller(answers: MovementAnswers) = new SummaryController(
+    SuccessfulAuth(),
+    ValidJourney(answers),
+    cache,
+    service,
     stubMessagesControllerComponents(),
     mockArrivalSummaryPage,
     mockDepartureSummaryPage
@@ -50,16 +50,12 @@ class SummaryControllerSpec extends LegacyControllerSpec with MockSubmissionServ
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
-
-    authorizedUser()
-    setupErrorHandler()
     when(mockArrivalSummaryPage.apply(any())(any(), any())).thenReturn(HtmlFormat.empty)
     when(mockDepartureSummaryPage.apply(any())(any(), any())).thenReturn(HtmlFormat.empty)
   }
 
   override protected def afterEach(): Unit = {
     reset(mockArrivalSummaryPage, mockDepartureSummaryPage)
-
     super.afterEach()
   }
 
@@ -70,70 +66,27 @@ class SummaryControllerSpec extends LegacyControllerSpec with MockSubmissionServ
     "return 200 (OK)" when {
 
       "cache contains data and user is during arrival journey" in {
-
-        givenAUserOnTheArrivalJourney()
-        withCacheMap(Some(CacheMap("id", Map.empty[String, JsValue])))
-
-        val result = controller.displayPage()(getRequest())
+        val result = controller(ArrivalAnswers()).displayPage()(getRequest())
 
         status(result) mustBe OK
       }
 
       "cache contains data and user is during departure journey" in {
-
-        givenAUserOnTheDepartureJourney()
-        withCacheMap(Some(CacheMap("id", Map.empty[String, JsValue])))
-
-        val result = controller.displayPage()(getRequest())
+        val result = controller(DepartureAnswers()).displayPage()(getRequest())
 
         status(result) mustBe OK
       }
 
       "submission service return ACCEPTED during submission" in {
-        givenAUserOnTheArrivalJourney()
-        mockSubmission()
-        mockCustomsCacheServiceClearedSuccessfully
+        when(service.submit(any(), any[MovementAnswers])(any())).thenReturn(Future.successful(ConsignmentReferences("ref", "value")))
 
-        val result = controller.submitMovementRequest()(postRequest(emptyForm))
+        val result = controller(ArrivalAnswers()).submitMovementRequest()(postRequest(emptyForm))
 
         status(result) mustBe SEE_OTHER
         redirectLocation(result) mustBe Some(controllers.routes.MovementConfirmationController.display().url)
         flash(result).get(FlashKeys.MOVEMENT_TYPE) mustBe Some(Choice.Arrival.value)
-        flash(result).get(FlashKeys.UCR_KIND) mustBe Some("D")
-        flash(result).get(FlashKeys.UCR) mustBe Some("5GB123456789000-123ABC456DEFIIII")
-      }
-    }
-
-    "return 500 (INTERNAL_SERVER_ERROR)" when {
-
-      "there is no data in cache" in {
-
-        givenAUserOnTheArrivalJourney()
-        withCacheMap(None)
-
-        val result = controller.displayPage()(getRequest())
-
-        status(result) mustBe INTERNAL_SERVER_ERROR
-      }
-
-      "user is on different type of journey" in {
-
-        withCaching(Choice.choiceId, Some(ShutMUCR))
-        withCacheMap(Some(CacheMap("id", Map.empty[String, JsValue])))
-
-        val result = controller.displayPage()(getRequest())
-
-        status(result) mustBe INTERNAL_SERVER_ERROR
-      }
-
-      "submission service returned different status than ACCEPTED" in {
-
-        givenAUserOnTheArrivalJourney()
-        mockSubmission(BAD_REQUEST)
-
-        val result = controller.submitMovementRequest()(postRequest(emptyForm))
-
-        status(result) mustBe INTERNAL_SERVER_ERROR
+        flash(result).get(FlashKeys.UCR_KIND) mustBe Some("ref")
+        flash(result).get(FlashKeys.UCR) mustBe Some("value")
       }
     }
   }

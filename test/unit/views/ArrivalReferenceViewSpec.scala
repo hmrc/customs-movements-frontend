4aff39e1bdf9cd4e5f67cbd227fc73ecf5ec1c01
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-12-13 15:26:41 +0000

    CEDS-2027 - arrival-reference using GDS - view spec

--- test/unit/views/ArrivalReferenceViewSpec.scala
+++ test/unit/views/ArrivalReferenceViewSpec.scala
@@ -1,76 +1,65 @@
-///*
-// * Copyright 2019 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package views
-//
-//import controllers.routes
-//import forms.ArrivalReference
-//import play.api.data.Form
-//import play.twirl.api.Html
-//import views.html.arrival_reference
-//import views.spec.UnitViewSpec
-//
-//class ArrivalReferenceViewSpec extends UnitViewSpec {
-//
-//  val arrivalReferencePage = ??? ///new arrival_reference(mainTemplate)
-//  def createView(form: Form[ArrivalReference] = ArrivalReference.form): Html =
-//    arrivalReferencePage(form)(request, messages)
-//
-//  "Arrival Reference messages" should {
-//
-//    "have correct content" in {
-//
-//      val messages = messagesApi.preferred(request)
-//
-//      messages must haveTranslationFor("arrivalReference")
-//      messages must haveTranslationFor("arrivalReference.question")
-//      messages must haveTranslationFor("arrivalReference.hint")
-//      messages must haveTranslationFor("arrivalReference.error")
-//    }
-//  }
-//
-//  "Arrival Reference page" should {
-//
-//    "display same page title as header" in {
-//
-//      val view = arrivalReferencePage(ArrivalReference.form)(request, messagesApi.preferred(request))
-//      view.title() must include(view.getElementsByTag("h1").text())
-//    }
-//
-//    "have question" in {
-//
-//      createView().getElementById("reference-label").text() mustBe messages("arrivalReference.question")
-//    }
-//
-//    "have hint" in {
-//
-//      createView().getElementById("reference-hint").text() mustBe messages("arrivalReference.hint")
-//    }
-//
-//    "have 'Continue' button" in {
-//
-//      createView().getElementById("submit").text() mustBe messages("site.continue")
-//    }
-//
-//    "have back button" in {
-//
-//      val backButton = createView().getElementById("back-link")
-//
-//      backButton must containMessage("site.back")
-//      backButton must haveHref(routes.ConsignmentReferencesController.displayPage())
-//    }
-//  }
-//}
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.Injector
+import forms.ArrivalReference
+import models.cache.ArrivalAnswers
+import play.api.data.Form
+import views.html.arrival_reference
+
+class ArrivalReferenceViewSpec extends ViewSpec with Injector {
+
+  private implicit val request = journeyRequest(ArrivalAnswers())
+
+  private val form: Form[ArrivalReference] = ArrivalReference.form
+  private val page = instanceOf[arrival_reference]
+
+  private def createPage = page(ArrivalReference.form, Some("some-reference"))
+
+  "Arrival Reference page" should {
+
+    "have the correct title" in {
+      createPage.getTitle must containMessage("arrivalReference.question")
+    }
+
+    "have the correct header" in {
+      createPage.getElementsByTag("h1").first() must containMessage("arrivalReference.question")
+    }
+
+    "have the correct section header" in {
+      createPage.getElementById("section-header") must containMessage("arrivalReference.sectionHeading", "some-reference")
+    }
+
+    "render the correct labels and hints" in {
+      createPage.getElementsByAttributeValue("for", "reference").first() must containMessage("arrivalReference.question")
+      createPage.getElementById("reference-hint") must containMessage("arrivalReference.hint")
+    }
+
+    "display 'Back' button that links to start page" in {
+      val backButton = createPage.getBackButton
+
+      backButton mustBe defined
+      backButton.get must haveHref(controllers.routes.ConsignmentReferencesController.displayPage())
+    }
+
+    "display 'Continue' button on page" in {
+      createPage.getElementsByClass("govuk-button").first() must containMessage("site.continue")
+    }
+
+  }
+}

9988266b6cff5a85ba34699933be6c2f510c80df
Mauricio Fernandes de Castro <mauricio.de.castro@digital.hmrc.gov.uk>
2019-12-20 12:02:04 +0000

    CEDS-2028 - Refactored shut mucr summary page.
    The reason why this is required is due to the existing frontend assets
    library being deprecated by HMRC and GDS and they are no longer
    accessible.

--- test/unit/views/ShutMucrSummaryViewSpec.scala
+++ test/unit/views/ShutMucrSummaryViewSpec.scala
@@ -16,53 +16,43 @@
 
 package views
 
+import base.Injector
 import controllers.consolidations.routes
 import forms.ShutMucr
 import helpers.views.CommonMessages
-import play.twirl.api.Html
+import models.cache.ShutMucrAnswers
+import models.requests.JourneyRequest
+import play.api.mvc.AnyContentAsEmpty
 import testdata.ConsolidationTestData.validMucr
 import views.html.shut_mucr_summary
-import views.spec.UnitViewSpec
 
-class ShutMucrSummaryViewSpec extends UnitViewSpec with CommonMessages {
+class ShutMucrSummaryViewSpec extends ViewSpec with CommonMessages with Injector {
 
-  private val shutMucrSummaryPage = new shut_mucr_summary(mainTemplate)
-  private val view: Html = shutMucrSummaryPage(ShutMucr(validMucr))(request, messages)
+  private val shutMucrSummaryPage = instanceOf[shut_mucr_summary]
+  private implicit val request: JourneyRequest[AnyContentAsEmpty.type] = journeyRequest(ShutMucrAnswers())
 
   "Shut Mucr Summary View" should {
 
-    "have proper labels for messages" in {
-
-      val messages = messagesApi.preferred(request)
-
-      messages must haveTranslationFor("shutMucr.summary.title")
-      messages must haveTranslationFor("shutMucr.summary.header")
-      messages must haveTranslationFor("shutMucr.summary.type")
-    }
-
-    "display page title" in {
-
-      view.getElementById("title").text() mustBe messages("shutMucr.summary.title")
-    }
+    val view = shutMucrSummaryPage(ShutMucr(validMucr))(request, messages)
 
     "display page header" in {
 
-      view.getElementById("shutMucr-header").text() mustBe messages("shutMucr.summary.header")
+      view.getElementsByClass("govuk-heading-m").text() mustBe messages("shutMucr.summary.header")
     }
 
-    "display MUCR type in table row" in {
+    "display MUCR type in summary list" in {
 
-      view.getElementById("shutMucr-type").text() mustBe messages("shutMucr.summary.type")
+      view.getElementsByClass("govuk-summary-list__key").text() mustBe messages("shutMucr.summary.type")
     }
 
     "display correct mucr" in {
 
-      view.getElementById("shutMucr-mucr").text() mustBe messages(validMucr)
+      view.getElementsByClass("govuk-summary-list__value").text() mustBe validMucr
     }
 
     "display correct change button" in {
 
-      val changeButton = view.getElementById("shutMucr-change")
+      val changeButton = view.getElementsByClass("govuk-link").first()
 
       changeButton must haveHref(routes.ShutMucrController.displayPage())
       changeButton.text() must include(messages("site.edit"))
@@ -70,9 +60,41 @@ class ShutMucrSummaryViewSpec extends UnitViewSpec with CommonMessages {
 
     "display correct submit button" in {
 
-      val submitButton = view.getElementById("submit")
+      val submitButton = view.getElementsByClass("govuk-button").first()
 
       submitButton.text() mustBe messages("site.confirmAndSubmit")
     }
   }
 }
+
+//@ViewTest
+//class DisassociateUcrSummaryViewSpec extends ViewSpec with CommonMessages with Injector {
+//
+//  private val page = instanceOf[disassociate_ucr_summary]
+//  private implicit val request: JourneyRequest[AnyContentAsEmpty.type] = journeyRequest(DisassociateUcrAnswers())
+//
+//
+//  "Disassociate Ucr Summary View" should {
+//
+//    val view = page(DisassociateUcr(Ducr, ducr = Some("SOME-DUCR"), mucr = None))(request, messages)
+//
+//    "display 'Confirm and submit' button on page" in {
+//      view.getElementsByClass("govuk-button").text() must be(messages(confirmAndSubmitCaption))
+//    }
+//
+//    "display 'Change' link on page" in {
+//      val changeButton = view.getElementsByClass("govuk-link").first()
+//      changeButton must containMessage(changeCaption)
+//      changeButton must haveAttribute(
+//        "href",
+//        controllers.consolidations.routes.DisassociateUcrController.displayPage().url
+//      )
+//    }
+//
+//    "display 'Reference' link on page" in {
+//      view.getElementsByClass("govuk-summary-list__value").first() must containText("SOME-DUCR")
+//    }
+//
+//  }
+//
+//}

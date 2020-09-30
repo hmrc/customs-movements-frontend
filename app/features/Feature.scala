2e75db77af7fd79eb2a0b4b073bffa4cadc3e5c9
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2020-09-30 13:17:04 +0100

    CEDS-2719 Implement feature flag for Beta Banner

--- app/features/Feature.scala
+++ app/features/Feature.scala
@@ -20,7 +20,7 @@ import play.api.mvc.PathBindable
 
 object Feature extends Enumeration {
   type Feature = Value
-  val default, ileQuery, ducrPart = Value
+  val default, ileQuery, ducrPart, betaBanner = Value
 
   implicit object featurePathStringBinder
       extends PathBindable.Parsing[Feature.Feature](

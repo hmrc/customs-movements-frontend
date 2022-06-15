d93cdc350a41914f7f006d0fce4f3c12e96e94a4
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2022-06-15 15:48:07 +0100

    CEDS-3910 Bump all dependencies to latest where possible

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,29 +6,29 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % "5.3.0",
-    "uk.gov.hmrc"          %% "logback-json-logger"           % "5.1.0",
+    "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % "5.24.0",
+    "uk.gov.hmrc"          %% "logback-json-logger"           % "5.2.0",
     "uk.gov.hmrc"          %% "govuk-template"                % "5.66.0-play-27",
-    "uk.gov.hmrc"          %% "play-allowlist-filter"         % "1.0.0-play-28",
-    "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.9.0-play-28",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.0.0-play-28",
-    "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.13.0-play-27",
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"            % "0.63.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.3",
+    "uk.gov.hmrc"          %% "play-allowlist-filter"         % "1.1.0",
+    "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.11.0-play-28",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.21.0-play-28",
+    "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.15.0-play-28",
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"            % "0.64.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.13.3",
     "ai.x"                 %% "play-json-extensions"          % "0.42.0",
-    "com.github.tototoshi" %% "scala-csv"                     % "1.3.6",
-    "org.webjars.npm"      %  "govuk-frontend"                % "3.12.0",
-    "org.webjars.npm"      %  "accessible-autocomplete"       % "2.0.3"
+    "com.github.tototoshi" %% "scala-csv"                     % "1.3.10",
+    "org.webjars.npm"      %  "govuk-frontend"                % "4.1.0",
+    "org.webjars.npm"      %  "accessible-autocomplete"       % "2.0.4"
   )
 
   val test = Seq(
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % "test, it",
-    "org.scalatest"          %% "scalatest"          % "3.2.9"             % "test, it",
+    "org.scalatest"          %% "scalatest"          % "3.2.12"             % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % "test, it",
-    "org.scalatestplus"      %% "mockito-3-4"        % "3.2.9.0"           % "test",
-    "com.vladsch.flexmark"   %  "flexmark-all"       % "0.36.8"            % "test, it",
-    "org.jsoup"              %  "jsoup"              % "1.13.1"            % "test, it",
-    "com.github.tomakehurst" %  "wiremock-jre8"      % "2.28.0"            % "test, it"
+    "org.scalatestplus"      %% "mockito-3-4"        % "3.2.10.0"           % "test",
+    "com.vladsch.flexmark"   %  "flexmark-all"       % "0.62.2"            % "test, it",
+    "org.jsoup"              %  "jsoup"              % "1.14.3"            % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"      % "2.33.2"            % "test, it"
   )
 
   def apply(): Seq[ModuleID] = (compile ++ test).map(_.withSources)

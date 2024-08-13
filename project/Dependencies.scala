268b83ab35cd118fd06d4247b81135af83c7aeda
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2024-08-13 14:04:46 +0100

    CEDS-5841 Small build improvements

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,6 @@ object Dependencies {
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapPlayVersion,
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % frontendPlayVersion,
-    "uk.gov.hmrc"           %% "play-allowlist-filter"                 % "1.3.0",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.1.0",
     "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.21.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,

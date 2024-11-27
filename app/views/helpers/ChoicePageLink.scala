40b3408a7c8acbb63ec658c2d35e0a255d0b7385
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-11-27 17:57:44 +0000

    CEDS-6203 WIP, added 'Arrive'

---
+++ app/views/helpers/ChoicePageLink.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.helpers
+
+import play.api.mvc.Call
+import play.twirl.api.Html
+import views.html.components.gds._
+
+import javax.inject.{Inject, Singleton}
+
+case class ChoicePageLink(title: String, description: String, linkText: String, link: Call)
+
+@Singleton
+class ChoicePageLinkHelper @Inject()(
+                                      heading: heading,
+                                      link: link
+                                    ) {
+
+  def generateChoiceOption(cpl: ChoicePageLink): Html = {
+    new Html(List(
+      heading(cpl.title, "govuk-heading-m", "h2"),
+      paragraphBody(cpl.description),
+    link(
+        message = Html(cpl.linkText),
+        href = cpl.link,
+        target = "_blank"
+      )))
+
+  }
+
+}

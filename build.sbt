9c4c95c13c3729b540a23332351da10df3011f09
sunil-sethumadhavan <171044301+sunil-sethumadhavan@users.noreply.github.com>
2025-04-17 16:14:20 +0100

    DCWL-3205: Dependencies upgrade (#481)
    * DCWL-3205: Dependencies upgrade
    
    * DCWL-3205: addressed PR bot comment for dependency update

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
 val appName = "customs-movements-frontend"
 
 ThisBuild / majorVersion := 0
-ThisBuild / scalaVersion := "2.13.15"
+ThisBuild / scalaVersion := "2.13.16"
 
 PlayKeys.devSettings := List("play.server.http.port" -> "6796")
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "9.5.0"
-  val frontendPlayVersion = "11.2.0"
-  val hmrcMongoVersion = "2.3.0"
+  val bootstrapPlayVersion = "9.11.0"
+  val frontendPlayVersion = "12.0.0"
+  val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapPlayVersion,
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % frontendPlayVersion,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.2.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.22.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.webjars.npm"       %  "accessible-autocomplete"               % "3.0.0",

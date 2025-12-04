bfdc371e2e2f39ad9efaa01cff07478a988cd823
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2025-12-04 14:22:19 +0000

    DCWL-3029 - Content not translated to Welsh (#491)
    DCWL-3075 - Error message is not prefixed with Error
    
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

---
+++ app/views/helpers/ErrorMapper.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.helpers
+
+import play.api.data.FormError
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.errormessage.ErrorMessage
+
+object ErrorMapper {
+
+  def prefixedErrorMessage(err: FormError)(implicit messages: Messages): ErrorMessage =
+    ErrorMessage(visuallyHiddenText = Some(messages("site.accessibility.error")), content = Text(messages(err.message, err.args: _*)))
+}

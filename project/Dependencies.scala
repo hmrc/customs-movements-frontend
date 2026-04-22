062aa1cfd177cd635a0263f07719ce02c81d57cd
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-04-22 16:47:32 +0100

    updated dependancies as per platops pr bot

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "10.5.0"
-  val frontendPlayVersion = "12.31.0"
+  val bootstrapPlayVersion = "10.7.0"
+  val frontendPlayVersion = "12.32.0"
   val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapPlayVersion,
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % frontendPlayVersion,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.4.0",
-    "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.23.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.5.0",
+    "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.24.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.webjars.npm"       %  "accessible-autocomplete"               % "3.0.1",
     "commons-codec"         %  "commons-codec"                         % "1.21.0",

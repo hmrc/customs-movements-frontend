2e759684f72f5c3401bec3d21f1d2e6325672f28
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-09-07 14:09:29 +0100

    DCWL-4867-update dependencies & repos.yaml

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "10.7.0"
-  val frontendPlayVersion = "13.9.0"
+  val bootstrapPlayVersion = "10.8.0"
+  val frontendPlayVersion = "13.12.0"
   val hmrcMongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = List(
@@ -13,7 +13,7 @@ object Dependencies {
     "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.24.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.webjars.npm"       %  "accessible-autocomplete"               % "3.0.1",
-    "commons-codec"         %  "commons-codec"                         % "1.22.0",
+    "commons-codec"         %  "commons-codec"                         % "1.22.1",
     "jakarta.xml.bind"      %  "jakarta.xml.bind-api"                  % "4.0.5"
   )
 
@@ -21,7 +21,7 @@ object Dependencies {
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapPlayVersion % "test",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % hmrcMongoVersion     % "test",
     "com.vladsch.flexmark"  %  "flexmark-all"            % "0.64.8"             % "test",
-    "org.jsoup"             %  "jsoup"                   % "1.22.2"             % "test",
+    "org.jsoup"             %  "jsoup"                   % "1.23.2"             % "test",
     "org.scalatestplus"     %% "mockito-5-12"            % "3.2.19.0"           % "test",
     "org.scalatest"         %% "scalatest"               % "3.2.20"             % "test",
   )
